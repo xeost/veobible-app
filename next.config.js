76cb976fba2e92943b630f55120aae3b7098a148
Fabián Karaben <fabiankaraben@gmail.com>
2026-05-30 09:22:39 -0300

    feat: integrate next-image-export-optimizer to handle static image generation and optimization

--- next.config.js
+++ next.config.js
@@ -14,11 +14,12 @@ const withPWA = require('next-pwa')({
 
 const nextConfig = {
   output: 'export',
+  transpilePackages: ["next-image-export-optimizer"],
   images: {
     loader: 'custom',
     loaderFile: './src/lib/imageLoader.ts',
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [],
+    deviceSizes: [384, 750],
   },
   env: {
     nextImageExportOptimizer_imageFolderPath: 'public/images',
